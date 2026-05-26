831b49ee1dd1c7a6250d68df42bb18444a643475
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-05-26 15:11:18 +0700

    initialize backend project with Gradle, Spring Boot, and Kotlin

---
+++ src/test/kotlin/com/vqn/bizflow/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vqn.bizflow.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
